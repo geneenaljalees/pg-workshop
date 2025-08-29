14fa8b3f06349ec915e1a4a28f83413b177c172b
Geneen <geneen850@gmail.com>
2025-08-29 15:29:51 +0300

    Add logging middleware to logs every request & error handling middleware to catch and handle errors ,Updated all route handlers to throw errors inside try/catch and forward them using next()

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
-const express = require("express");
-const { join } = require("path");
-const router = require("./controllers");
+const express = require('express');
+const { join } = require('path');
+const router = require('./controllers');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(join(__dirname, "..", "public")));
-
-app.set("port", process.env.PORT || 5000);
+app.use(express.static(join(__dirname, '..', 'public')));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} at ${new Date().toISOString()}`);
+  next();
+});
+app.set('port', process.env.PORT || 5000);
 
 app.use(router);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 module.exports = app;
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,14 +4,16 @@ const {
   deleteUser,
 } = require('../database/queries/user.queries.js');
 
-
-
+// middleware execute before every route handeler 
 const fetchUsers = async (req, res, next) => {
   try {
     const result = await getUsers();
+      if (!result) {
+        throw new Error('Database connection failed!');
+      }
     res.json(result.rows);
   } catch (err) {
-    next(err);
+    next(err); //must be Error Middleware
   }
 };
 
@@ -22,6 +24,9 @@ const addUser = async (req, res, next) => {
       return res.status(400).json({ error: 'Name and location are required.' });
     }
     const result = await createUser(name, location);
+      if (!result) {
+        throw new Error('Database connection failed!');
+      }
     res.status(201).json(result.rows[0]);
   } catch (err) {
     next(err);
@@ -32,6 +37,9 @@ const removeUser = async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await deleteUser(id);
+      if (!result) {
+        throw new Error('Database connection failed!');
+      }
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'User not found.' });
     }
